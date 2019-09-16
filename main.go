cae140d9253b026dc38eb1f1bd6bc8fb247f6afa
rxml <352242180@qq.com>
2019-09-16 16:55:19 +0800

    222

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	_ "apigo/routers"
 	"github.com/astaxie/beego"
-	"fmt"
 	"apigo/config"
 )
 
@@ -30,48 +29,8 @@ func (iPhone IPhone) call() {
 }
 
 func main() {
-	/*if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}*/
-	//递增
-	const (
-		a = iota
-		b
-		c
-		d
-	)
-	fmt.Println(a,b,c,d)
-	var f = 1
-	var h = &f
-	fmt.Println(h,*h)
-	var aa [10]int
-	var i int
-	for i = 0; i < 10; i++ {
-		aa[i] = i + 100
-	}
-	fmt.Println(aa)
-	cc := aa[:3] 
-	fmt.Println(cc)
-
-	//切片
-	var j,k int
-	qiePian := make([][]int, 3)
-	for j = 0; j < 3; j++ {
-		len := j+1
-		qiePian[j] = make([]int, len)
-		for k = 0; k < len; k++ {
-			qiePian[j][k] = j+k
-		}
-	}
-	fmt.Println("2d: ", qiePian)
-	
-	var p Phone
-	p = new (NokiaPhone)
-	p.call()
-
-	p = new (IPhone)
-	p.call()
+	//链接数据库
 	config.InitMysql()
+	//启动
 	beego.Run()
 }
